d7408f14fc6f3f8f92c5a78883b331d89d534e18
Oliver Krause <3621164+okrause@users.noreply.github.com>
2021-02-08 14:05:09 +0100

    update-ActiveDirectory.py: Some cleanup and improvements

--- update-ActiveDirectory.py
+++ update-ActiveDirectory.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
 # Update CVS Active Directory entries in GCP
+# Usage: python3 update-ActiveDirectory.py <region>
+# set environment variables first: 
+# - SERVICE_ACCOUNT_CREDENTIAL_FILE
+# - DEVSHELL_PROJECT_NUMBER
 
-import google.auth
 import google.auth.transport.requests
 import requests
 import json
 from os import getenv, path
-from sys import exit
+from sys import exit, argv
 from google.auth import jwt
-from google.oauth2 import service_account
-from google.oauth2 import id_token
+from google.oauth2 import service_account, id_token
 
 # Set this variables for your environment
 service_account_file = getenv('SERVICE_ACCOUNT_CREDENTIAL_FILE', None)
 project_number = getenv('DEVSHELL_PROJECT_NUMBER', None)
-region = "europe-west4"
+if len(argv) == 2:
+    region = argv[1]
+else:
+    region = "europe-west3"
 
 # Internal variables
 audience = 'https://cloudvolumesgcp-api.netapp.com'
@@ -22,7 +27,7 @@ server = 'https://cloudvolumesgcp-api.netapp.com'
 
 # check if DEVSHELL_PROJECT_NUMBER is set
 if project_number == None:
-    print('ERROR: Project number not set.')
+    print('ERROR: Project number not set in DEVSHELL_PROJECT_NUMBER.')
     exit(1)
 else:
     print("INFO: Project number: ", project_number)
@@ -30,6 +35,7 @@ else:
 # check if file with service account credentials exists
 if not path.isfile(service_account_file):
     print('ERROR: Service account credential file "' + service_account_file + '" cannot be found.')
+    print('Please set environment variable SERVICE_ACCOUNT_CREDENTIAL_FILE')
     exit(1)
 
 # Get all volumes from all regions
@@ -59,29 +65,46 @@ r = requests.get(get_url, headers=headers)
 r.raise_for_status()
 
 print("Active Directories:")
-updated=False
+found=False
 for ad in r.json():
     if ad["region"] == region:
         
         print("Found AD data:")
-        print(json.dumps(ad, indent=4, sort_keys=False))
-        keys_to_keep = ['DNS', 'domain', 'ldapSigning', 'netBIOS', 'organizationalUnit', 'region', 'username']
+        print(json.dumps(ad, indent=4, sort_keys=True))
+        keys_to_keep = ['DNS',
+                        'domain',
+                        'ldapSigning',
+                        'netBIOS',
+                        'organizationalUnit',
+                        'region',
+                        'username',
+                        'backupOperators',
+                        'securityOperators']
         uuid = ad["UUID"]
+ 
         newad = dict()
         # only keep relevant keys
         for key in keys_to_keep:
-            newad[key] = ad[key]
+            if key in ad:
+                newad[key] = ad[key]
+
         # add new keys we want to set
-        newad["site"] = "Default-First-Site-Name"
-        newad["backupOperators"] = [""]
+        # newad["site"] = "Default-First-Site-Name"#
+        newad["backupOperators"] = ["svc-GFC"]
+
+        # password will not be returned by GET call. You have to set it again
         newad["password"] = "xxx"
-        # print(newad)
+
+        # print(json.dumps(newad, indent=4, sort_keys=True))
 
         # update AD
         put_url = server + "/v2/projects/" + str(project_number) + "/locations/" + region + "/Storage/ActiveDirectory/" + uuid
-        #r2 = requests.put(put_url, data=json.dumps(newad), headers=headers)
-        #print(json.dumps(r2.json(), indent=4))
-        updated=True
+        r2 = requests.put(put_url, data=json.dumps(newad), headers=headers)
+        print("New AD settings:")
+        print(json.dumps(r2.json(), indent=4))
 
-if not updated:
+        found=True
+        break
+
+if not found:
     print("No AD entry for region " + region + " found.")
